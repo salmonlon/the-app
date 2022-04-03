e97b15e90eb236f454538a7657b2208ad5c1da73
Sean Xiang <sean.xiang@sydney.edu.au>
2022-04-03 14:28:27 +1000

    add middlewares

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+import time
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dependencies import get_query_token
 from routers import items
 
@@ -7,6 +9,22 @@ app = FastAPI(
     # dependencies=[Depends(get_query_token)]
     )
 app.include_router(items.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.middleware("http")
+async def add_process_time_header(request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 @app.get("/")
 async def root():
