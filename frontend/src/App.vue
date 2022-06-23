7e731c6ae747d011706a16effd4c30f4d0954a2b
Sean Xiang <sean.xiang@sydney.edu.au>
2022-06-23 21:38:19 +1000

    update navbar reference

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
     <NavBar />
-    <router-view/>
+    <div class="main container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
-
 <script>
 // @ is an alias to /src
-import NavBarVue from "@/components/NavBar.vue";
-import NavBar from "./components/NavBar.vue";
-export default{
+import NavBar from '@/components/NavBar.vue'
+export default {
   components: {
-    NavBarVue,
     NavBar
-}
+  }
 }
 </script>
 
@@ -25,9 +24,7 @@ export default{
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-
 .main {
   padding-top: 5em;
 }
-
 </style>
