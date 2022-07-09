1bdb026430df8e2305322ce07ffa4e2dc09bd080
Sean Xiang <sean.xiang@sydney.edu.au>
2022-07-09 20:01:15 +1000

    add axios interceptor

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,6 +13,18 @@ axios.defaults.baseURL = 'http://localhost:5001/'
 
 Vue.config.productionTip = false
 
+// axios interceptors, redirects to login page if 401 unauthorized
+axios.interceptors.request.use(undefined, function (error) {
+  if (error) {
+    const originalReqesut = error.config;
+    if (error.response.status === 401 && !originalReqesut._retry) {
+      originalReqesut._retry = true;
+      store.dispatch('logout');
+      router.push('/login');
+    }
+  }
+});
+
 new Vue({
   router,
   store,
